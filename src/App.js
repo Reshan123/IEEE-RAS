b23651581558c2c9281be24bc4c4048610fbc69c
Haritha Himansha <143889589+hhimansha@users.noreply.github.com>
2024-07-18 00:05:48 +0530

    chore: Add initial project files and dependencies

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Team from "./components/Team";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Hero/>
+      <About/>
+      <Team/>
+      <Contact/>
+      {/* <Footer/> */}
+    </div>
+  );
+}
+
+export default App;
